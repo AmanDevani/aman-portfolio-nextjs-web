f39f15dd489c0fc7c59cdd364a8930a6a16d8123
AmanDevani <amandevani25@gmail.com>
2026-08-08 11:52:53 +0530

    removed all files

--- app/(home)/page.tsx
+++
@@ -1,9 +0,0 @@
-import Home from "@/components/pages/home";
-
-export default function page() {
-  return (
-    <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-      <Home />
-    </div>
-  );
-}
